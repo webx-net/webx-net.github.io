ebcdeb662ec5a4599e04cd5f0592a0c582fdf243
WilliamRagstad <william.ragstad@gmail.com>
2024-08-03 13:51:52 +0200

    Transparent header always

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
 
 	return (
 		<nav
-			className="w-full h-20 flex flex-col justify-center items-center fixed bg-bgDark1 lg:bg-bgDarkTransparent z-40 lg:backdrop-blur-xl"
+			className="w-full h-20 flex flex-col justify-center items-center fixed bg-bgDark1 bg-bgDarkTransparent z-40 backdrop-blur-xl"
 			aria-label="Main navigation"
 		>
 			<div className="2xl:w-[1280px] xl:w-10/12 w-11/12 flex justify-between items-center relative">
