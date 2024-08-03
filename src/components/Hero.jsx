af7ea3d576ecf95a7153b8d4d2983d1e49b81fee
WilliamRagstad <william.ragstad@gmail.com>
2024-08-03 21:04:54 +0200

    Make margins smaller on phones

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,10 +11,10 @@ export const Hero = () => {
 
 	return (
 		<section
-			className="w-screen flex justify-center items-center bg-bgDark1 mb-[28vw] md:mb-[18vw] lg:mb-[10vw] xl:mb-[13vw] 2xl:mb-60 pb-24 sm:pb-32 md:pb-44 lg:pb-0"
+			className="w-screen flex justify-center items-center bg-bgDark1 mb-[28vw] md:mb-[18vw] lg:mb-[10vw] xl:mb-[13vw] 2xl:mb-60 pb-12 sm:pb-32 md:pb-44 lg:pb-0"
 			id="home"
 			style={{
-				backgroundImage: `url(${darkFlowing.src}), url(${lightFlowing.src})`,
+				backgroundImage: `url(${darkFlowing.src})`,
 				backgroundPosition: "center",
 				backgroundSize: "cover",
 				backgroundRepeat: "no-repeat",
@@ -26,7 +26,7 @@ export const Hero = () => {
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ duration: 0.5 }}
 				>
-					<h3 className="text-secondaryColor text-sm sm:text-base mb-6 mt-16 sm:mt-32 lg:mt-52 font-bold font-Bungee">
+					<h3 className="text-secondaryColor text-sm sm:text-base mb-4 sm:mb-6 mt-16 sm:mt-32 lg:mt-52 font-bold font-Bungee">
 						{/* Discover new data flow */}
 						Unleash the Power of Hypermedia
 					</h3>
@@ -58,7 +58,7 @@ export const Hero = () => {
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ duration: 0.5, delay: 0.1 }}
 				>
-					<h2 className="text-secondaryText text-sm lg:text-base xl:text-lg sm:text-base mt-10 px-12 sm:px-48 ">
+					<h2 className="text-secondaryText text-sm lg:text-base xl:text-lg sm:text-base mt-6 sm:mt-10 px-12 sm:px-48 ">
 						{/* Experience seamless business integrations and powerful insights with
 						our cutting-edge analytic tools. */}
 						A superset of <em>TypeScript</em> running on the <em>V8</em> engine in a hyper-efficient <em>Rust</em> web server environment.
@@ -69,7 +69,7 @@ export const Hero = () => {
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ duration: 0.5, delay: 0.15 }}
 				>
-					<div className="flex flex-col gap-2 sm:flex-row mt-14 mb-24 sm:mb-40 justify-center">
+					<div className="flex flex-col gap-2 sm:flex-row mt-6 sm:mt-14 mb-12 sm:mb-48 justify-center">
 						<button
 							className="contained-button w-64 sm:w-52 h-12 mr-0 sm:mr-4 lg:mr-6 mb-2 sm:mb-0"
 							onClick={() => setIsModalOpen(true)}
