52de6c713a6174f9cd027c3bd404d1d06cb4ef46
WilliamRagstad <william.ragstad@gmail.com>
2024-08-04 17:44:39 +0200

    Improve testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,30 +1,38 @@
 import { motion } from "framer-motion";
 
 import { QuoteIcon } from "../assets/icons/QuoteIcon";
-import testimonial1 from "../assets/images/testimonial1.png";
-import testimonial2 from "../assets/images/testimonial2.png";
-import testimonial3 from "../assets/images/testimonial3.png";
+import testimonial1 from "../assets/images/testimonial/person1.webp";
+import testimonial2 from "../assets/images/testimonial/person2.jpg";
+import testimonial3 from "../assets/images/testimonial/person3.png";
 
 const testimonialsData = [
 	{
-		customerName: "John Watkins",
-		customerTitle: "Founder of Dashflow",
+		customerName: "Andrew Jones",
+		customerTitle: "Senior Developer",
 		content:
-			"The powerful analytic tools have helped us streamline our processes and make data-driven decisions that positively impact our efficiency. Web X has been a game-changer for our business. The platform is easy to use, and the insights we've gained have driven significant improvements.",
+			// `The powerful analytic tools have helped us streamline our processes and make data-driven decisions that positively impact our efficiency.
+			// Web X has been a game-changer for our business. The platform is easy to use, and the insights we've gained have driven significant improvements.`,
+			`I was skeptical about switching frameworks, but Web X exceeded all my expectations.
+It was incredibly easy to get started and integrate with our existing systems, and the performance boost was noticeable immediately.
+Web X made development smoother and more enjoyable!`,
 		image: testimonial1,
 	},
 	{
-		customerName: "John Watkins",
-		customerTitle: "Founder of Dashflow",
+		customerName: "Mattias Larsson",
+		customerTitle: "Tech Lead",
 		content:
-			"The powerful analytic tools have helped us streamline our processes and make data-driven decisions that positively impact our efficiency. Web X has been a game-changer for our business. The platform is easy to use, and the insights we've gained have driven significant improvements.",
+			`Web X has significantly sped up our development cycles.
+			The simplified workflow and powerful features saved us countless hours.
+			Thanks to the active community and comprehensive documentation onboarding was a breeze.
+			It's a game-changer for backend development.`,
 		image: testimonial2,
 	},
 	{
-		customerName: "John Watkins",
-		customerTitle: "Founder of Dashflow",
-		content:
-			"The powerful analytic tools have helped us streamline our processes and make data-driven decisions that positively impact our efficiency. Web X has been a game-changer for our business. The platform is easy to use, and the insights we've gained have driven significant improvements.",
+		customerName: "Marc Jacobs",
+		customerTitle: "Technology Officer",
+		content: `We faced constant frustrations with our previous backend system, and tried transition to Web X.
+		The developer experience is fantastic with its intuitive syntax and carefully designed features.
+		Our backend is now faster, more scalable, and a joy to work with thanks to Web X!`,
 		image: testimonial3,
 	},
 ];
@@ -41,7 +49,7 @@ export const Testimonials = () => (
 			>
 				<div className="block-subtitle text-center mb-6">Testimonials</div>
 				<div className="block-big-title text-center mb-20 px-8 sm:px-24 md:px-48">
-					People like you love Web X
+					People have tried it
 				</div>
 
 				<div className="flex flex-col lg:flex-row gap-8 lg:gap-5 xl:gap-10 px-6 xl:px-0 items-center">
@@ -61,6 +69,7 @@ export const Testimonials = () => (
 										alt="Customer avatar"
 										width="45px"
 										height="5px"
+										className="rounded-full border border-white/40"
 										aria-label={testimonial.customerName}
 									/>
 								</div>
