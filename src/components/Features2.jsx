c4ad5ef08e9e35b8993b6166b7351dfd8a02b78b
WilliamRagstad <william.ragstad@gmail.com>
2024-08-04 19:13:27 +0200

    Add images

--- src/components/Features2.jsx
+++ src/components/Features2.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 
 import feature5 from "../assets/images/template/feature5.jpg";
 import feature6 from "../assets/images/template/feature6.jpg";
+import hands from "../assets/images/stock/hands.webp";
 import { GithubIcon } from "../assets/icons/GithubIcon";
 
 export const Features2 = () => (
@@ -17,15 +18,15 @@ export const Features2 = () => (
 				<div className="w-11/12 sm:w-3/4 mx-auto lg:w-1/2 flex flex-wrap lg:-mx-4 sm:pr-8 justify-center order-last lg:order-first">
 					<div className="mb-8 lg:mb-0 w-full px-2 lg:pl-16 flex flex-col justify-center md:pl-8">
 						<div className="mb-4 py-3 md:pl-3 md:pr-20 lg:pr-12 rounded">
-							<img
-								src={feature5.src}
-								alt="Feature image 5"
-								className="rounded-xl  main-border-gray"
-							/>
+							<div className="rounded-xl main-border-gray">
+								<img src="https://api.star-history.com/svg?repos=webx-net/webx&type=Date"
+									alt="Star history"
+									className="rounded-xl invert hue-rotate-180 contrast-[73%] saturate-150" />
+							</div>
 						</div>
 						<div className="py-3 md:pl-20 lg:pl-12 md:pr-2 rounded ">
 							<img
-								src={feature6.src}
+								src={hands.src}
 								alt="Feature image 6"
 								className="rounded-xl  main-border-gray"
 							/>
