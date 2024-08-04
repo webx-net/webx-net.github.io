626e7da04eec0589c2db3532269e0754ad5942af
WilliamRagstad <william.ragstad@gmail.com>
2024-08-04 16:42:38 +0200

    Add icons

---
+++ src/assets/icons/RightArrowIcon.jsx
@@ -0,0 +1,6 @@
+export const RightArrowIcon = () => (<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 32 32"
+	xml:space="preserve">
+	<path d="M27.728,16.024l-8.485,8.482l-2.828-2.835L20.071,18H6v-4h14.071l-3.657-3.644l2.828-2.816  L27.728,16.024z" />
+</svg>);
